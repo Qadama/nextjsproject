ae6865cd2456a598cbe28c74751eb2849cf439ad
Qadama <gradesup12@gmail.com>
2025-05-26 11:29:35 +0500

    Added build files with .next folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             We Are Developers Team Here For Assistance, You Can Contact Us Feel Free Hahaha This is Very Dangerous Code We Are Ready To Provide Query Parameters 
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly. without reuploading the code from cpanel again and again..</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -33,7 +33,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            See More Now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
