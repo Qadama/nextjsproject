cb9fd57ab6ee10cbbe93af69db78ca2587568fd3
Qadama <gradesup12@gmail.com>
2025-05-26 12:11:03 +0500

    Added build files with .next folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-           Hey There, This is Testing Project For Testing Ci Cd Pipeline Feature
+           Hey There, This is Testing Project For Testing Ci Cd Pipeline Feature, do you ready to test it with auto node app restarting and auto code updated..
           </li>
           <li>Save and see your changes instantly. without reuploading the code from cpanel again and again..</li>
         </ol>
